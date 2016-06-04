0cdeecde5750e0a84a7bf680f714ad357050d5d3
Tom Wang <tom.wanghaiyi@gmail.com>
2016-06-04 22:18:57 +0800

    Edit todos

--- todo/scripts/app.js
+++ todo/scripts/app.js
@@ -1,4 +1,6 @@
-var app = angular.module('todo', []);
+var app = angular.module('todo', [
+  'ngAnimate'
+]);
 
 app.controller('TodoController', function($scope) {
     
@@ -7,18 +9,56 @@ app.controller('TodoController', function($scope) {
   var data = JSON.parse(localStorage.getItem('todos'));
   $scope.todos = data || [];
   
+  $scope.todos = $scope.todos.map(function(todo) {
+    return {
+      text: todo.text,
+      done: todo.done || false,
+      editing: false
+    }
+  });
+  
   $scope.addTodo = function() {
-    $scope.todos.push({ text: $scope.todo });
-    $scope.todo = '';
     
-    var data = JSON.stringify($scope.todos);
-    localStorage.setItem('todos', data);
-
+    $scope.todo = $scope.todo.trim();
+    if ($scope.todo.length === 0) return;
+    
+    $scope.todos.push({ 
+      text: $scope.todo,
+      done: false 
+    });
+    $scope.todo = '';
+    saveTodos();
   };
   
   $scope.removeTodo = function(index) {
     $scope.todos.splice(index, 1);
+    saveTodos();
   };
+  
+  $scope.editTodo = function(todo) {
+    $scope.todos.forEach((todo) => { todo.editing = false; });
+    todo.editing = true;
+  };
+  
+  $scope.updateTodo = function(todo, index) {
+    saveTodos();
+    todo.editing = false;
+  };
+  
+  $scope.doneTodo = function(todo) {
+    saveTodos();
+  };
+  
+  function saveTodos() {
+    var todos = $scope.todos.map(function(todo) {
+      return { 
+        text: todo.text,
+        done: todo.done 
+      };
+    });
+    
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }
   
   
 });
